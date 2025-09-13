96a7b7115c3b9bd3dd366f90e0fcdf6761052b44
Pkoka99 <zekazeus123@gmail.com>
2025-09-14 01:10:47 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -17,7 +17,7 @@ def run_workflow(message):
     url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/actions/workflows/{WORKFLOW_FILE}/dispatches"
 
     headers = {
-        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Authorization": f"Token {GITHUB_TOKEN}",
         "Accept": "application/vnd.github+json"
     }
 
